29e1ee0344dcd61dd465c05b8fd56aa297e8d310
shiwani <shiwanipaunikar1@gmail.com>
2023-02-08 21:46:49 -0800

    Added:Mood Analyser Program

---
+++ src/main/java/com/mood/analyzer/MoodAnalyserProgram.java
@@ -0,0 +1,7 @@
+package com.mood.analyzer;
+
+public class MoodAnalyserProgram{
+    public static void main(String[] args) {
+        System.out.println("Welcome to Mood Analyser Program");
+    }
+}
